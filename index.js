5929c37439eff1cee8288d56d918c2a669b2d279
angel chavez <chavezangel62@gmail.com>
2024-09-18 20:27:32 -0600

    create if,else if functions

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400) {
+    return "This one is on me!"
+  } else if (someValue > 400 && someValue <= 2000 ) {
+    return "That will be twenty bucks."
+  } else if (someValue > 2000 && someValue <2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }  
+  }
+
+function ternaryCheckCity(someCity){
+  if (someCity == "NYC") {
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(money){
+  if (money == "generous") {
+    return "Thank you so much."
+  } else if (money == "not as generous") {
+    return "Thank you." 
+  } else {
+    return "Bye."
+  }
 }
